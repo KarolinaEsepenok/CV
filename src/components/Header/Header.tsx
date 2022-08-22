95f0ab0307998aabbb2ac329b44265fcd6c04d7c
karolinasmile@mail.ru <karolinasmile@mail.ru>
2022-08-22 21:05:04 +0300

    firstCommit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+<a href={''}>Главная</a>
+            <a href={'../Skills/Skills.tsx'}>Скиллы</a>
+            <a href={'../MyWorks/MyWorks.tsx'}>Работы</a>
+            <a href={'../Contacts/Contacts.tsx'}>Контакты</a>
+        </div>
+    );
+};
+
+export default Header;
