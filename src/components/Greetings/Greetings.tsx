efabdfda5c0f42b18a7f8f1f14334b6e6fc5ad30
karolinasmile@mail.ru <karolinasmile@mail.ru>
2022-11-08 20:19:01 +0300

    animaCommit

--- src/components/Greetings/Greetings.tsx
+++ src/components/Greetings/Greetings.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import avatar from '../img/avatar.jpg'
 import s from './Greetings.module.css'
+import 'animate.css'
+
 const Greetings = () => {
     return (
         <div className={s.greetings}>
-            <h2>Привет!<br/> Меня зовут Каролина.<br/> Я front-end разработчик</h2>
+          <h2 className={'animate__lightSpeedInRight'}>Привет!<br/> Меня зовут Каролина.<br/> Я front-end разработчик</h2>
  <img src={avatar} alt={'Avatar'} title={'Karolina Esepenok'}/>
         </div>
 
