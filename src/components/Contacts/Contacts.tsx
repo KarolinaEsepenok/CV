8277c29e8c88917c58fe651a49258cd79a09e258
karolinasmile@mail.ru <karolinasmile@mail.ru>
2023-01-29 17:18:42 +0300

    emailjs add

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import s from './Contacts.module.scss'
-
-import Fade, {AttentionSeeker} from "react-awesome-reveal";
+import emailjs from '@emailjs/browser'
 
 const Contacts = () => {
+    const form = useRef<HTMLInputElement>(null)
+    const sendEmail = (e:any) => {
+        e.preventDefault()
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    emailjs.sendForm('service_rfu75tm','template_trkuazi',form.current,'fYLlGpL8DB4s59SmU')
+        .then((result) =>{
+            console.log(result.text)
+        }),(error:any)=>{
+        console.log(error.text)
+    }
+
+
     return (
 
-        <div  className={s.contact}><p>Contacts</p>
-
-           <form className={s.formMain}>
-               <input type={'text'} className={s.formArea} placeholder={'Name'}/>
-               <input type={'text'}className={s.formArea} placeholder={'E-mail'}/>
-               <textarea  className={s.messageArea} placeholder={'Message'} />
-               <button type={'submit'} className={s.submitBtn}>Send</button>
-           </form>
+        <div className={s.contact}><p>Contacts</p>
+            <form ref={form} onSubmit={sendEmail} className={s.formMain} >
+                <input type={'text'} className={s.formArea} placeholder={'name'}
+                       />
+                <input type={'text'} className={s.formArea} placeholder={'E-mmail'}
+                      />
+                <textarea className={s.messageArea} placeholder={'Message'}
+                      />
+                <button type={'submit'} className={s.submitBtn}>Send</button>
+            </form>
 
         </div>
     );
